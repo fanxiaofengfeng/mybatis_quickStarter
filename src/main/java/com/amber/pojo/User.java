6362fe7f76c6514c0cc50447a3f471843acaa8fb
fanxiaofeng <fanxiaofeng@qianlima.com>
2023-07-06 16:31:46 +0800

    初始化mybatis

---
+++ src/main/java/com/amber/pojo/User.java
@@ -0,0 +1,16 @@
+package com.amber.pojo;
+
+import lombok.Data;
+
+/**
+ * @author
+ * @version 1.0
+ * @date 2023/7/6
+ * @description
+ */
+@Data
+public class User {
+    private int id;
+    private String username;
+    private String password;
+}
